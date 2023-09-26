561ae53faa92b89d5f0edbaff872def8dc9735a3
gsharad007 <gsharad007@hotmail.com>
2023-09-26 17:44:00 -0400

    Add player movement functionality
    This commit introduces player movement functionality to the game. The player can now move in all four directions (up, down, left, right) using either the arrow keys or the WASD keys. The movement speed is set to a constant value of 500.0 units per second. The direction of movement is normalized to ensure consistent speed in all directions. The player's position is updated based on the direction of movement, speed, and the elapsed time since the last frame.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use bevy::window::PrimaryWindow;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, (spawn_camera, spawn_player))
+        .add_systems(Update, player_movement)
         .run();
 }
 
@@ -39,4 +39,33 @@ fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Primar
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        direction = direction.normalize_or_zero();
+
+        let delta_time = time.delta_seconds();
+        const SPEED: f32 = 500.0;
+        player_transform.translation += direction * SPEED * delta_time;
+    }
 }
